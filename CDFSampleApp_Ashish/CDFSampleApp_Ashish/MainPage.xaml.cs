c5e3bb0be69ebb7d43cf30a44396e880bb7c08b1
ashjain <ashishj@microsoft.com>
2016-08-16 10:31:03 -0700

    Initial Commit

---
+++ CDFSampleApp_Ashish/CDFSampleApp_Ashish/MainPage.xaml.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Identity.Provider;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CDFSampleApp_Ashish
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        String m_selectedDeviceId = String.Empty;
+        bool taskRegistered = false;
+        static string myBGTaskName = "myBGTask";
+        static string myBGTaskEntryPoint = "Tasks.myBGTask";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DeviceListBox.SelectionChanged += DeviceListBox_SelectionChanged;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            IReadOnlyList<SecondaryAuthenticationFactorInfo> deviceList = await SecondaryAuthenticationFactorRegistration.FindAllRegisteredDeviceInfoAsync(SecondaryAuthenticationFactorDeviceFindScope.User);
+
+            RefreshDeviceList(deviceList);
+
+        }
+
+        void RefreshDeviceList(IReadOnlyList<SecondaryAuthenticationFactorInfo> deviceList)
+        {
+            DeviceListBox.Items.Clear();
+
+            for (int index = 0; index < deviceList.Count; ++index)
+            {
+                SecondaryAuthenticationFactorInfo deviceInfo = deviceList.ElementAt(index);
+                DeviceListBox.Items.Add(deviceInfo.DeviceId);
+            }
+        }
+
+        private async void RegisterDevice_Click(object sender, RoutedEventArgs e)
+        {
+            String deviceId = System.Guid.NewGuid().ToString();
+
+            // WARNING: Test code
+            // These keys should be generated on the companion device
+            // Create device key and authentication key
+            IBuffer deviceKey = CryptographicBuffer.GenerateRandom(32);
+            IBuffer authKey = CryptographicBuffer.GenerateRandom(32);
+
+            //
+            // WARNING: Test code
+            // The keys SHOULD NOT be saved into device config data
+            //
+            byte[] deviceKeyArray = { 0 };
+            CryptographicBuffer.CopyToByteArray(deviceKey, out deviceKeyArray);
+
+            byte[] authKeyArray = { 0 };
+            CryptographicBuffer.CopyToByteArray(authKey, out authKeyArray);
+
+            //Generate combinedDataArray
+            int combinedDataArraySize = deviceKeyArray.Length + authKeyArray.Length;
+            byte[] combinedDataArray = new byte[combinedDataArraySize];
+            for (int index = 0; index < deviceKeyArray.Length; index++)
+            {
+                combinedDataArray[index] = deviceKeyArray[index];
+            }
+            for (int index = 0; index < authKeyArray.Length; index++)
+            {
+                combinedDataArray[deviceKeyArray.Length + index] = authKeyArray[index];
+            }
+
+            // Get a Ibuffer from combinedDataArray
+            IBuffer deviceConfigData = CryptographicBuffer.CreateFromByteArray(combinedDataArray);
+
+            //
+            // WARNING: Test code
+            // The friendly name and device model number SHOULD come from device
+            //
+            String deviceFriendlyName = "Test Simulator";
+            String deviceModelNumber = "Sample A1";
+
+            SecondaryAuthenticationFactorDeviceCapabilities capabilities = SecondaryAuthenticationFactorDeviceCapabilities.SecureStorage;
+
+            SecondaryAuthenticationFactorRegistrationResult registrationResult = await SecondaryAuthenticationFactorRegistration.RequestStartRegisteringDeviceAsync(deviceId,
+                    capabilities,
+                    deviceFriendlyName,
+                    deviceModelNumber,
+                    deviceKey,
+                    authKey);
+
+            if (registrationResult.Status != SecondaryAuthenticationFactorRegistrationStatus.Started)
+            {
+                MessageDialog myDlg = null;
+                
+                if (registrationResult.Status == SecondaryAuthenticationFactorRegistrationStatus.DisabledByPolicy)
+                {
+                    //For DisaledByPolicy Exception:Ensure secondary auth is enabled.
+                    //Use GPEdit.msc to update group policy to allow secondary auth
+                    //Local Computer Policy\Computer Configuration\Administrative Templates\Windows Components\Microsoft Secondary Authentication Factor\Allow Companion device for secondary authentication
+                    myDlg = new MessageDialog("Disabled by Policy.  Please update the policy and try again.");
+                }
+
+                if (registrationResult.Status == SecondaryAuthenticationFactorRegistrationStatus.PinSetupRequired)
+                {
+                    //For PinSetupRequired Exception:Ensure PIN is setup on the device
+                    //Either use gpedit.msc or set reg key
+                    //This setting can be enabled by creating the AllowDomainPINLogon REG_DWORD value under the HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System Registry key and setting it to 1.
+                    myDlg = new MessageDialog("Please setup PIN for your device and try again.");
+                }
+
+                if (myDlg != null)
+                {
+                    await myDlg.ShowAsync();
+                    return;
+                }
+            }
+
+            System.Diagnostics.Debug.WriteLine("Device Registration Started!");
+            await registrationResult.Registration.FinishRegisteringDeviceAsync(deviceConfigData);
+
+            DeviceListBox.Items.Add(deviceId);
+            System.Diagnostics.Debug.WriteLine("Device Registration is Complete!");
+
+            IReadOnlyList<SecondaryAuthenticationFactorInfo> deviceList = await SecondaryAuthenticationFactorRegistration.FindAllRegisteredDeviceInfoAsync(
+                SecondaryAuthenticationFactorDeviceFindScope.User);
+
+            RefreshDeviceList(deviceList);
+        }
+
+        private void DeviceListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DeviceListBox.Items.Count > 0)
+            {
+                m_selectedDeviceId = DeviceListBox.SelectedItem.ToString();
+            }
+            else
+            {
+                m_selectedDeviceId = String.Empty;
+            }
+            System.Diagnostics.Debug.WriteLine("The device " + m_selectedDeviceId + " is selected.");
+
+            //Store the selected device in settings to be used in the BG task
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values["SelectedDevice"] = m_selectedDeviceId;
+
+        }
+
+        private async void UnregisterDevice_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_selectedDeviceId == String.Empty)
+            {
+                return;
+            }
+
+            //InfoList.Items.Add("Unregister a device:");
+
+            await SecondaryAuthenticationFactorRegistration.UnregisterDeviceAsync(m_selectedDeviceId);
+
+            //InfoList.Items.Add("Device unregistration is completed.");
+
+            IReadOnlyList<SecondaryAuthenticationFactorInfo> deviceList = await SecondaryAuthenticationFactorRegistration.FindAllRegisteredDeviceInfoAsync(
+                SecondaryAuthenticationFactorDeviceFindScope.User);
+
+            RefreshDeviceList(deviceList);
+        }
+
+
+        void RegisterBgTask_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterTask();
+        }
+
+
+        private async void OnBgTaskProgress(BackgroundTaskRegistration sender, BackgroundTaskProgressEventArgs args)
+        {
+            // WARNING: Test code
+            // Handle background task progress.
+            if (args.Progress == 1)
+            {
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    System.Diagnostics.Debug.WriteLine("Background task is started.");
+                });
+            }
+        }
+
+        async void RegisterTask()
+        {
+            System.Diagnostics.Debug.WriteLine("Register the background task.");
+            //
+            // Check for existing registrations of this background task.
+            //
+
+            BackgroundExecutionManager.RemoveAccess();
+            var access = await BackgroundExecutionManager.RequestAccessAsync();
+
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == myBGTaskName)
+                {
+                    taskRegistered = true;
+                    break;
+                }
+            }
+
+            if (!taskRegistered)
+            {
+
+                if (access == BackgroundAccessStatus.AllowedSubjectToSystemPolicy)
+                {
+                    BackgroundTaskBuilder taskBuilder = new BackgroundTaskBuilder();
+                    taskBuilder.Name = myBGTaskName;
+                    // Create the trigger.
+                    SecondaryAuthenticationFactorAuthenticationTrigger myTrigger = new SecondaryAuthenticationFactorAuthenticationTrigger();
+
+                    taskBuilder.TaskEntryPoint = myBGTaskEntryPoint;
+                    taskBuilder.SetTrigger(myTrigger);
+                    BackgroundTaskRegistration taskReg = taskBuilder.Register();
+
+                    String taskRegName = taskReg.Name;
+                    //taskReg.Progress += OnBgTaskProgress;
+                    System.Diagnostics.Debug.WriteLine("Background task registration is completed.");
+                    taskRegistered = true;
+                }
+            }
+
+        }
+    }
+}
---
+++ CDFSampleApp_Ashish/Tasks/myBGTask.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Core;
+using Windows.Data.Xml.Dom;
+using Windows.Security.Authentication.Identity.Provider;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Notifications;
+
+namespace Tasks
+{
+    public sealed class myBGTask : IBackgroundTask
+    {
+        ManualResetEvent opCompletedEvent = null;
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+
+            // This event is signaled when the operation completes
+            opCompletedEvent = new ManualResetEvent(false);
+            SecondaryAuthenticationFactorAuthentication.AuthenticationStageChanged += OnStageChanged;
+            ShowToastNotification("BG Task Hit!");
+
+            // Wait until the operation completes
+            opCompletedEvent.WaitOne();
+
+            deferral.Complete();
+        }
+
+        async void PerformAuthentication()
+        {
+            ShowToastNotification("Performing Auth!");
+
+            //Get the selected device from app settings
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            String m_selectedDeviceId = localSettings.Values["SelectedDevice"] as String;
+
+            SecondaryAuthenticationFactorAuthenticationStageInfo authStageInfo = await SecondaryAuthenticationFactorAuthentication.GetAuthenticationStageInfoAsync();
+
+            if (authStageInfo.Stage != SecondaryAuthenticationFactorAuthenticationStage.CollectingCredential)
+            {
+                ShowToastNotification("Unexpected!");
+                throw new Exception("Unexpected!");
+            }
+
+            ShowToastNotification("Post Collecting Credential");
+
+            IReadOnlyList <SecondaryAuthenticationFactorInfo> deviceList = await SecondaryAuthenticationFactorRegistration.FindAllRegisteredDeviceInfoAsync(
+                    SecondaryAuthenticationFactorDeviceFindScope.AllUsers);
+
+            if (deviceList.Count == 0)
+            {
+                ShowToastNotification("Unexpected exception, device list = 0");
+                throw new Exception ("Unexpected exception, device list = 0");
+            }
+
+            ShowToastNotification("Found companion devices");
+
+            SecondaryAuthenticationFactorInfo deviceInfo = deviceList.ElementAt(0);
+            m_selectedDeviceId = deviceInfo.DeviceId;
+
+            ShowToastNotification("Device ID: " + m_selectedDeviceId);
+
+            //a nonce is an arbitrary number that may only be used once - a random or pseudo-random number issued in an authentication protocol to ensure that old communications cannot be reused in replay attacks.
+            IBuffer svcNonce = CryptographicBuffer.GenerateRandom(32);  //Generate a nonce and do a HMAC operation with the nonce
+                                                 
+
+            //In real world, you would need to take this nonce and send to companion device to perform an HMAC operation with it
+            //You will have only 20 second to get the HMAC from the companion device
+            SecondaryAuthenticationFactorAuthenticationResult authResult = await SecondaryAuthenticationFactorAuthentication.StartAuthenticationAsync(
+                    m_selectedDeviceId, svcNonce);
+
+            if (authResult.Status != SecondaryAuthenticationFactorAuthenticationStatus.Started)
+            {
+                ShowToastNotification("Unexpected! Could not start authentication!");
+                throw new Exception("Unexpected! Could not start authentication!");
+            }
+
+            ShowToastNotification("Auth Started");
+
+            //
+            // WARNING: Test code
+            // The HAMC calculation SHOULD be done on companion device
+            //
+            byte[] combinedDataArray;
+            CryptographicBuffer.CopyToByteArray(authResult.Authentication.DeviceConfigurationData, out combinedDataArray);
+
+            byte[] deviceKeyArray = new byte[32];
+            byte[] authKeyArray = new byte[32];
+            for (int index = 0; index < deviceKeyArray.Length; index++)
+            {
+                deviceKeyArray[index] = combinedDataArray[index];
+            }
+            for (int index = 0; index < authKeyArray.Length; index++)
+            {
+                authKeyArray[index] = combinedDataArray[deviceKeyArray.Length + index];
+            }
+            // Create device key and authentication key
+            IBuffer deviceKey = CryptographicBuffer.CreateFromByteArray(deviceKeyArray);
+            IBuffer authKey = CryptographicBuffer.CreateFromByteArray(authKeyArray);
+
+            // Calculate the HMAC
+            MacAlgorithmProvider hMACSha256Provider = MacAlgorithmProvider.OpenAlgorithm(MacAlgorithmNames.HmacSha256);
+
+            CryptographicKey deviceHmacKey = hMACSha256Provider.CreateKey(deviceKey);
+            IBuffer deviceHmac = CryptographicEngine.Sign(deviceHmacKey, authResult.Authentication.DeviceNonce);
+
+            // sessionHmac = HMAC(authKey, deviceHmac || sessionNonce)
+            IBuffer sessionHmac;
+            byte[] deviceHmacArray = { 0 };
+            CryptographicBuffer.CopyToByteArray(deviceHmac, out deviceHmacArray);
+
+            byte[] sessionNonceArray = { 0 };
+            CryptographicBuffer.CopyToByteArray(authResult.Authentication.SessionNonce, out sessionNonceArray);
+
+            combinedDataArray = new byte[deviceHmacArray.Length + sessionNonceArray.Length];
+            for (int index = 0; index < deviceHmacArray.Length; index++)
+            {
+                combinedDataArray[index] = deviceHmacArray[index];
+            }
+            for (int index = 0; index < sessionNonceArray.Length; index++)
+            {
+                combinedDataArray[deviceHmacArray.Length + index] = sessionNonceArray[index];
+            }
+
+            // Get a Ibuffer from combinedDataArray
+            IBuffer sessionMessage = CryptographicBuffer.CreateFromByteArray(combinedDataArray);
+
+            // Calculate sessionHmac
+            CryptographicKey authHmacKey = hMACSha256Provider.CreateKey(authKey);
+            sessionHmac = CryptographicEngine.Sign(authHmacKey, sessionMessage);
+
+            ShowToastNotification("Before finish auth");
+
+            SecondaryAuthenticationFactorFinishAuthenticationStatus authStatus = await authResult.Authentication.FinishAuthenticationAsync(deviceHmac,
+                sessionHmac);
+
+            if (authStatus != SecondaryAuthenticationFactorFinishAuthenticationStatus.Completed)
+            {
+                ShowToastNotification("Unable to complete authentication!");
+                throw new Exception("Unable to complete authentication!");
+            }
+
+            ShowToastNotification("Auth completed");
+        }
+
+        public static void ShowToastNotification(string message)
+        {
+
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            // Set Text
+            XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode(message));
+
+            // Set image
+            // Images must be less than 200 KB in size and smaller than 1024 x 1024 pixels.
+            XmlNodeList toastImageAttributes = toastXml.GetElementsByTagName("image");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("src", "ms-appx:///Images/logo-80px-80px.png");
+            ((XmlElement)toastImageAttributes[0]).SetAttribute("alt", "logo");
+
+            // toast duration
+            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "short");
+
+            // toast navigation
+            var toastNavigationUriString = "#/MainPage.xaml?param1=12345";
+            var toastElement = ((XmlElement)toastXml.SelectSingleNode("/toast"));
+            toastElement.SetAttribute("launch", toastNavigationUriString);
+
+            // Create the toast notification based on the XML content you've specified.
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            // Send your toast notification.
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+        }
+
+        // WARNING: Test code
+        // This code should be in background task
+        async void OnStageChanged(Object sender, SecondaryAuthenticationFactorAuthenticationStageChangedEventArgs args)
+        {
+            //ShowToastNotification("In StageChanged!" + args.StageInfo.Stage.ToString());
+            if (args.StageInfo.Stage == SecondaryAuthenticationFactorAuthenticationStage.WaitingForUserConfirmation)
+            {
+                ShowToastNotification("Stage = WaitingForUserConfirmation");
+                // This event is happening on a ThreadPool thread, so we need to dispatch to the UI thread.
+                // Getting the dispatcher from the MainView works as long as we only have one view.
+                String deviceName = "DeviceSimulator.";
+                await SecondaryAuthenticationFactorAuthentication.ShowNotificationMessageAsync(
+                    deviceName,
+                    SecondaryAuthenticationFactorAuthenticationMessage.SwipeUpWelcome);
+            }
+            else if (args.StageInfo.Stage == SecondaryAuthenticationFactorAuthenticationStage.CollectingCredential)
+            {
+                ShowToastNotification("Stage = CollectingCredential");
+
+                PerformAuthentication();
+            }
+            else
+            {
+                if (args.StageInfo.Stage == SecondaryAuthenticationFactorAuthenticationStage.StoppingAuthentication)
+                {
+                    SecondaryAuthenticationFactorAuthentication.AuthenticationStageChanged -= OnStageChanged;
+                    opCompletedEvent.Set();
+                }
+
+                SecondaryAuthenticationFactorAuthenticationStage stage = args.StageInfo.Stage;
+            }
+        }
+    }
+}
